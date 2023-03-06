392483c520399a5507682a230a5c10f4b6ed740a
Jack White <jack.white@team.belong.com.au>
2023-03-06 23:39:30 +1100

    Added fetch function with parameters

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = 3000;
+
+app.get("/:input", async (req, res) => {
+  const input = req.params.input;
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      max_tokens: 20,
+      messages: [
+        {
+          role: "user",
+          content: input,
+        },
+      ],
+    }),
+  });
+  const data = await response.json();
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
